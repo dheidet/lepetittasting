8a64835d8af2ef03206838d1d2c77ed938e9f574
dheidet <dimitri@36quaidesaromes.fr>
2017-03-11 14:45:34 +0100

    wine model

---
+++ db/migrate/20170311134322_create_wines.rb
@@ -0,0 +1,16 @@
+class CreateWines < ActiveRecord::Migration[5.0]
+  def change
+    create_table :wines do |t|
+      t.reference :estate
+      t.string :name
+      t.string :label
+      t.date :vintage
+      t.string :grape_variety
+      t.string :color
+      t.integer :mark
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
