69f463083d25afcfe1b1070fe20edabb7ce42434
dheidet <dimitri@36quaidesaromes.fr>
2017-03-12 18:17:56 +0100

    add estate CRUD

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -1,14 +1,47 @@
 class EstatesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:index, :new, :create, :edit, :update]
+
+  def index
+    @estates = Estate.all
+    authorize @estates
+  end
 
   def new
     @estate = Estate.new
-    @submit_label = "Ajouter mon vin"
+    @submit_label = "Ajouter le domaine"
     authorize @estate
   end
 
   def create
     @estate = Estate.new(estate_params)
+    authorize @estate
+    if @estate.save
+      flash[:notice] = "Domaine créée"
+      redirect_to estates_path
+    else
+      flash[:alert] = "Ça a planté, mec!"
+      render :new
+    end
+  end
+
+  def edit
+    @estate = Estate.find(params[:id])
+    @submit_label = "Modifier le domaine"
+    authorize @estate
+  end
+
+  def update
+    @estate = Estate.find(params[:id])
+    @submit_label = "Modifier le domaine"
+    @estate.update(estate_params)
+    authorize @estate
+    if @estate.save
+      flash[:notice] = "Update ok!!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Mais c'est pas bien rempli, tout ça!!"
+      render :edit
+    end
   end
 
   private
--- app/policies/estate_policy.rb
+++ app/policies/estate_policy.rb
@@ -4,7 +4,18 @@ class EstatePolicy < ApplicationPolicy
       scope
     end
   end
+  def index?
+    true
+  end
   def create?
     true
+  end
+  def update?
+    user_is_owner?
+  end
+
+  private
+  def user_is_owner?
+    record.user == user
   end
 end
